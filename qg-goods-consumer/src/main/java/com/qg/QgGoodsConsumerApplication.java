5ef24155eb5f2bf5f553d584d415ae5445aa75cd
xiepanpan <xiepanpan@thunisoft.com>
2019-10-15 17:38:10 +0800

    商品消费者

---
+++ qg-goods-consumer/src/main/java/com/qg/QgGoodsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.qg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QgGoodsConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QgGoodsConsumerApplication.class, args);
+    }
+
+}
